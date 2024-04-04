47899e643006b26485ba73cfa1e825567fb0d300
DK <laancer4@gmail.com>
2024-04-04 21:56:23 +0900

    init

---
+++ src/main/java/com/example/architecture_playground/ArchitecturePlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.architecture_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitecturePlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchitecturePlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/architecture_playground/ArchitecturePlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.architecture_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitecturePlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
